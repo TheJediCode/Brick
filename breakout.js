9822821daff97d685ebbb20721007453542e7289
TheJediCode <68616265+TheJediCode@users.noreply.github.com>
2020-08-16 17:40:46 -0400

    Added bricks

--- breakout.js
+++ breakout.js
@@ -18,6 +18,22 @@ let paddleX = (canvas.width - paddleWidth) / 2;
 let rightPressed = false;
 let leftPressed = false;
 
+let brickRowCount = 4;
+let brickColumnCount = 5;
+let brickWidth = 75;
+let brickHeight = 20;
+let brickPadding = 10;
+let brickOffsetTop = 30;
+let brickOffsetLeft = 30;
+let bricks = [];
+
+for (let c=0; c<brickColumnCount; c++) {
+    bricks[c] = [];
+    for (let r=0; r<brickRowCount; r++) {
+        bricks[c][r] = {x: 0, y: 0 }
+    }
+}
+
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
@@ -54,10 +70,45 @@ function drawPaddle() {
     ctx.closePath();
 }
 
+function drawBricks() {
+    for (let c=0; c<brickColumnCount; c++) {
+        for (let r=0; r<brickRowCount; r++) {
+            let brickX = (c*(brickWidth + brickPadding)) + brickOffsetLeft;
+            let brickY = (r*(brickHeight + brickPadding)) + brickOffsetTop;
+            bricks[c][r].x = brickX;
+            bricks[c][r].y = brickY;
+            ctx.beginPath();
+            ctx.rect(brickX, brickY, brickWidth, brickHeight);
+            ctx.fillStyle = "#0095DD";
+            ctx.fill();
+            ctx.closePath();
+        }
+    }
+}
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
     drawPaddle();
+    drawBricks();
+
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if (y + dy < ballRadius ) {
+        dy = -dy;
+        //interval -= setInterval(draw, 10 - .5);
+    } 
+    else if (y + dy > canvas.height - ballRadius) {
+        if (x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+        }
+        else {
+            alert("GAME OVER");
+            document.location.reload();
+            clearInterval(interval);
+        }
+    }
 
     if (rightPressed) {
         paddleX += 4;
@@ -81,16 +132,6 @@ function draw() {
         }
     });
     
-    
-
-    if ( y + dy > canvas.height - ballRadius || y + dy < ballRadius ) {
-        dy = -dy;
-    }
-
-    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
-        dx = -dx;
-    }
-
     x += dx;
     y += dy;
 }
